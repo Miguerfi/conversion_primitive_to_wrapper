26d46ec16e3131ede4249f25adc86aa1b52d75ac
Miguerfi <miguelpintojose00@gmail.com>
2024-02-15 18:43:58 -0300

    convertendo uma variavel com o tipo primitivo para um objeto.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    /**
+     * Wrapper Conversion
+     *
+     * @description Convert a primitive boolean to a Boolean object. wrapper class.
+     */
+    public static void main(String[] args) {
+        boolean status = Boolean.TRUE;
+
+        System.out.println("After Wrapper Class: " + status);
+
+    }
+}
